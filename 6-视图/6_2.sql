c4b772d143a29ea2a78e88a7c540ef530b3f48e4
yaochenxuan <deadfffool@gmail.com>
2023-05-05 17:04:29 +0800

    finish lab6

--- 6-视图/6_2.sql
+++ 6-视图/6_2.sql
@@ -4,6 +4,4 @@ select c_name,c_id_card, sum(i_amount * pro_quantity) as insurance_total_amount,
 from v_insurance_detail
 group by c_id_card
 order by insurance_total_amount desc;
-
-
 /*  end  of  your code  */
