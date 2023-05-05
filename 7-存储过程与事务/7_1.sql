91765a35d6b1c815d54c5c65339c41911e03a65c
yaochenxuan <deadfffool@gmail.com>
2023-05-05 18:00:33 +0800

    finish lab7

--- 7-存储过程与事务/7_1.sql
+++ 7-存储过程与事务/7_1.sql
@@ -7,17 +7,19 @@ delimiter $$
 create procedure sp_fibonacci(in m int)
 begin
 ######## 请补充代码完成存储过程体 ########
-    declare i bigint default 0;
-    declare j bigint default 1;
-    declare k bigint default 0;
-    declare t bigint default 0;
-    while k < m do
-    insert into fibonacci(n, fibn) value(k, i);
-    set t = i + j;
-    set i = j;
-    set j = t;
-    set k = k+1;
-    end while;
+  DECLARE a INT DEFAULT 0;
+  DECLARE b INT DEFAULT 1;
+  DECLARE i INT DEFAULT 0;
+  
+  TRUNCATE TABLE fibonacci;
+  
+  WHILE i < m DO
+    INSERT INTO fibonacci VALUES (i, a);
+    SET b = a + b;
+    SET a = b - a;
+    SET i = i + 1;
+  END WHILE;
+
 
 end $$
 
